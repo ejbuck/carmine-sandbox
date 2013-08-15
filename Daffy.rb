f152b3998b94ecb13d4033ed2a07a5bf92ec6aa2
Emily J. Buck <Buck.Emily.J@gmail.com>
2013-08-14 17:12:11 -0700

    Practice program from codecademy.com with a few tweaks.

---
+++ Daffy.rb
@@ -0,0 +1,16 @@
+def daffy
+    print "Thring, pleathe!"
+    user_input = gets.chomp
+    user_input.downcase!
+
+    if user_input.include? "s"
+        user_input.gsub!(/s/, "th")
+        puts "Now it's #{user_input}. Much better!"
+    elsif user_input.length == 0
+        puts "Hey, joker, I thee what you did there."
+        daffy
+    else
+    puts "That'th fine."
+end
+
+daffy()
