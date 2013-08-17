d279c8acf45d2ca8b853d035cc4324ab5a15f2e9
Emily J. Buck <Buck.Emily.J@gmail.com>
2013-08-17 00:04:22 -0700

    Exercise from Codecademy plus tweaks. Can't figure out how to have multiple redactions work...YET.

---
+++ HammerandNail.rb
@@ -0,0 +1,13 @@
+=begin
+I want a list of numbers adding up to the goal amount returned.
+
+Maybe it would be easier to do ANY number that adds up to the goal amount, within certain bounds? Hmm. That would mean generating the arrays and evaluating them in the same step. Maybe not, at least in this first go 'round.
+=end
+
+def bell (total, *nums)
+    for item in num_list:
+    
+
+
+
+
