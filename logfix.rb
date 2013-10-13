fd9560bfa2c1c328a5e2dd6460a16818336f4fb0
Emily J. Buck <Buck.Emily.J@gmail.com>
2013-10-13 13:37:53 -0700

    Refactored MORE.

--- logfix.rb
+++ logfix.rb
@@ -6,14 +6,9 @@ per day, cross reference, etc.
 require 'csv'
 
 @csv = CSV.read("/home/clarity/Desktop/Bed Log.csv")
-@list = []
 @hash = {}
 
-@csv.each { |row| @list << row.to_a}
-
-@list.each { |entry| k = entry[0]; v = entry[1]; @hash[k] = v.split("\n") }
-
-#@hash.each {|k,v| @splithash[k] = v.split("\n")}
+@csv.each {|row| entry = row.to_a; @hash[entry[0]] = entry[1].split("\n")}
 
 print @hash
 puts ""
